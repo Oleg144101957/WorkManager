afc805b6a81ac83e8b10876525801d54128d4b3f
Oleg <vishnevskiyoleg@gmail.com>
2022-06-15 04:47:10 +0300

    aaaa

--- app/src/main/java/com/vishnevskiypro/workmanager/MyWorker.kt
+++ app/src/main/java/com/vishnevskiypro/workmanager/MyWorker.kt
@@ -3,7 +3,6 @@ package com.vishnevskiypro.workmanager
 import android.content.Context
 import android.os.Environment
 import android.util.Log
-import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import java.io.File
